d8bc5cfd41f33cfba3eaee4fe4d7e1a6a345562a
anandaftrr <anandafitria.unand@gmail.com>
2022-09-19 14:45:28 +0700

    detail kp

--- app/src/main/java/com/example/kelompok3optimisa/DetailKP.java
+++ app/src/main/java/com/example/kelompok3optimisa/DetailKP.java
@@ -2,13 +2,34 @@ package com.example.kelompok3optimisa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
 
 public class DetailKP extends AppCompatActivity {
+
+    ImageButton BtnBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_kp);
+
+        BtnBack = findViewById(R.id.btn_back);
+
+        BtnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(DetailKP.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/kelompok3optimisa/MainActivity.java
+++ app/src/main/java/com/example/kelompok3optimisa/MainActivity.java
@@ -7,14 +7,27 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageButton BtnLogbook;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BtnLogbook = findViewById(R.id.btn_logbook);
+
+        BtnLogbook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, DetailKP.class);
+                startActivity(intent);
+            }
+        });
     }
 }
