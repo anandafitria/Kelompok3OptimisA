9391477c670a0400f968dff4e1efd715eb78c5e1
anandaftrr <anandafitria.unand@gmail.com>
2022-09-23 15:24:23 +0700

    nyambungin layout dashboard

--- app/src/main/java/com/example/kelompok3optimisa/ListLogbook.java
+++ app/src/main/java/com/example/kelompok3optimisa/ListLogbook.java
@@ -2,13 +2,46 @@ package com.example.kelompok3optimisa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class ListLogbook extends AppCompatActivity {
+
+    ImageButton BtnHome, BtnListSeminar, BtnProfil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_logbook);
+
+        BtnHome = findViewById(R.id.btn_home);
+        BtnListSeminar = findViewById(R.id.btn_seminar);
+        BtnProfil = findViewById(R.id.btn_profil);
+
+        BtnHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent home = new Intent(ListLogbook.this, MainActivity.class);
+                startActivity(home);
+            }
+        });
+
+        BtnListSeminar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent listseminar = new Intent(ListLogbook.this, ListSeminar.class);
+                startActivity(listseminar);
+            }
+        });
+
+        BtnProfil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent profil = new Intent(ListLogbook.this, ProfilActivity.class);
+                startActivity(profil);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/kelompok3optimisa/ProfilActivity.java
+++ app/src/main/java/com/example/kelompok3optimisa/ProfilActivity.java
@@ -2,13 +2,46 @@ package com.example.kelompok3optimisa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class ProfilActivity extends AppCompatActivity {
+
+    ImageButton BtnHome, BtnListLogbook, BtnListSeminar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profil);
+
+        BtnHome = findViewById(R.id.btn_home);
+        BtnListLogbook = findViewById(R.id.btn_logbook);
+        BtnListSeminar = findViewById(R.id.btn_seminar);
+
+        BtnHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent home = new Intent(ProfilActivity.this, MainActivity.class);
+                startActivity(home);
+            }
+        });
+
+        BtnListLogbook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent listlogbook = new Intent(ProfilActivity.this, ListLogbook.class);
+                startActivity(listlogbook);
+            }
+        });
+
+        BtnListSeminar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent listseminar = new Intent(ProfilActivity.this, ListSeminar.class);
+                startActivity(listseminar);
+            }
+        });
     }
 }
