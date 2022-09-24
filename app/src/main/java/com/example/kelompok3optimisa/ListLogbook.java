e08263e4a18fcebf9607d7ed8a0924ad4abb8c64
Aisyah2011521018 <91660844+Aisyah2011521018@users.noreply.github.com>
2022-09-24 18:32:49 +0700

    bismillah

--- app/src/main/java/com/example/kelompok3optimisa/ListLogbook.java
+++ app/src/main/java/com/example/kelompok3optimisa/ListLogbook.java
@@ -1,20 +1,29 @@
 package com.example.kelompok3optimisa;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 
+import java.util.ArrayList;
+
 public class ListLogbook extends AppCompatActivity {
 
+    private RecyclerView rvLogbook;
     ImageButton BtnHome, BtnListSeminar, BtnProfil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_logbook);
+
+        rvLogbook = findViewById(R.id.rv_logbook);
+        rvLogbook.setHasFixedSize(true);
+
 
         BtnHome = findViewById(R.id.btn_home);
         BtnListSeminar = findViewById(R.id.btn_seminar);
---
+++ app/src/main/java/com/example/kelompok3optimisa/LogbookMahasiswa.java
@@ -0,0 +1,31 @@
+package com.example.kelompok3optimisa;
+
+public class LogbookMahasiswa {
+    private String nama;
+    private String nim;
+    private String foto;
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNim() {
+        return nim;
+    }
+
+    public void setNim(String nim) {
+        this.nim = nim;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+}
