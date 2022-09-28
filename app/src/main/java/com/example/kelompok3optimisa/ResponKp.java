fd1a166cbdd42ba4a0fddad07e8569ca7408cd5c
daeng1502 <daengfebrino01@gmail.com>
2022-09-28 21:18:13 +0700

    Update Edit Toast

--- app/src/main/java/com/example/kelompok3optimisa/ResponKp.java
+++ app/src/main/java/com/example/kelompok3optimisa/ResponKp.java
@@ -1,12 +1,10 @@
 package com.example.kelompok3optimisa;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class ResponKp extends AppCompatActivity {
 
@@ -14,15 +12,13 @@ public class ResponKp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_respon_kp);
-
-    }
-    public void button3 (View view)
-    {
-        Toast.makeText(this, "Berhasil Diupdate", Toast.LENGTH_SHORT).show();
-    }
-    public void button4 (View view)
-    {
-        Toast.makeText(this, "Berhasil Diedit", Toast.LENGTH_SHORT).show();
     }
 
+    public void updateOnClick(View view) {
+        Toast.makeText(ResponKp.this, "Berhasil Diupdate", Toast.LENGTH_SHORT).show();
+    }
+
+    public void editOnClick(View view) {
+        Toast.makeText(ResponKp.this, "Berhasil Diedit", Toast.LENGTH_SHORT).show();
+    }
 }
