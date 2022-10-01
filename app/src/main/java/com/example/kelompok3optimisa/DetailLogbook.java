f99b209a149f8d5c2f61be10f10cebf4e3bf561d
anandaftrr <anandafitria.unand@gmail.com>
2022-10-01 10:57:13 +0700

    detail logbook

--- app/src/main/java/com/example/kelompok3optimisa/DetailLogbook.java
+++ app/src/main/java/com/example/kelompok3optimisa/DetailLogbook.java
@@ -4,17 +4,52 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class DetailLogbook extends AppCompatActivity {
 
     private TextView NamaLogbook, NIMLogbook;
     private String yNamaLogbook, yNIMLogbook;
+    ImageButton BtnHome, BtnListLogbook, BtnListSeminar, BtnProfil, BtnBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_logbook);
+
+        BtnHome = findViewById(R.id.btn_home);
+        BtnListLogbook = findViewById(R.id.btn_logbook);
+        BtnListSeminar = findViewById(R.id.btn_seminar);
+        BtnProfil = findViewById(R.id.btn_profil);
+        BtnBack = findViewById(R.id.btn_back);
+
+        BtnHome.setOnClickListener(view -> {
+            Intent home = new Intent(DetailLogbook.this, MainActivity.class);
+            startActivity(home);
+        });
+
+        BtnListLogbook.setOnClickListener(view -> {
+            Intent listlogbook = new Intent(DetailLogbook.this, ListLogbook.class);
+            startActivity(listlogbook);
+        });
+
+        BtnListSeminar.setOnClickListener(view -> {
+            Intent listseminar = new Intent(DetailLogbook.this, ListSeminar.class);
+            startActivity(listseminar);
+        });
+
+        BtnProfil.setOnClickListener(view -> {
+            Intent profil = new Intent(DetailLogbook.this, ProfilActivity.class);
+            startActivity(profil);
+        });
+
+        BtnBack.setOnClickListener(view -> {
+            Intent back = new Intent(DetailLogbook.this, MainActivity.class);
+            startActivity(back);
+        });
+
 
         initView();
 
--- app/src/main/java/com/example/kelompok3optimisa/InputNilai.java
+++ app/src/main/java/com/example/kelompok3optimisa/InputNilai.java
@@ -9,21 +9,43 @@ import android.widget.ImageButton;
 
 public class InputNilai extends AppCompatActivity {
 
-    ImageButton BtnBack;
+    ImageButton BtnHome, BtnListLogbook, BtnListSeminar, BtnProfil, BtnBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input_nilai);
 
+        BtnHome = findViewById(R.id.btn_home);
+        BtnListLogbook = findViewById(R.id.btn_logbook);
+        BtnListSeminar = findViewById(R.id.btn_seminar);
+        BtnProfil = findViewById(R.id.btn_profil);
         BtnBack = findViewById(R.id.btn_back);
 
-        BtnBack.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent back = new Intent(InputNilai. this, DetailDashboard.class);
-                startActivity(back);
-            }
+        BtnHome.setOnClickListener(view -> {
+            Intent home = new Intent(InputNilai.this, MainActivity.class);
+            startActivity(home);
         });
+
+        BtnListLogbook.setOnClickListener(view -> {
+            Intent listlogbook = new Intent(InputNilai.this, ListLogbook.class);
+            startActivity(listlogbook);
+        });
+
+        BtnListSeminar.setOnClickListener(view -> {
+            Intent listseminar = new Intent(InputNilai.this, ListSeminar.class);
+            startActivity(listseminar);
+        });
+
+        BtnProfil.setOnClickListener(view -> {
+            Intent profil = new Intent(InputNilai.this, ProfilActivity.class);
+            startActivity(profil);
+        });
+
+        BtnBack.setOnClickListener(view -> {
+            Intent back = new Intent(InputNilai.this, MainActivity.class);
+            startActivity(back);
+        });
+
     }
 }
