b7c62b6a47f306de38fc169c0c9e3a09277fbd75
Aisyah2011521018 <91660844+Aisyah2011521018@users.noreply.github.com>
2022-12-08 03:00:04 +0700

    notif batal kp, input nilai, login

--- app/src/main/java/com/example/kelompok3optimisa/InputNilai.java
+++ app/src/main/java/com/example/kelompok3optimisa/InputNilai.java
@@ -1,15 +1,27 @@
 package com.example.kelompok3optimisa;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 public class InputNilai extends AppCompatActivity {
 
+    private static final String CHANNEL_ID = "Inputnilai_kanal";
     ImageButton BtnHome, BtnProfil, BtnBack, BtnListLogbook;
+    private Button btn_seminarkp;
+    private NotificationManagerCompat notificationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,5 +57,50 @@ public class InputNilai extends AppCompatActivity {
             startActivity(back);
         });
 
+        //1. Ambil notifikationManager
+        notificationManager = NotificationManagerCompat.from(this);
+
+        //2b Buat channel notifikasi
+        createNotificationChannel();
+
+        btn_seminarkp = findViewById(R.id.btn_seminarkp);
+        btn_seminarkp.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent resultIntent = new Intent(InputNilai.this, InputNilai.class);
+                TaskStackBuilder stackBuilder = TaskStackBuilder.create(InputNilai.this);
+                stackBuilder.addNextIntentWithParentStack(resultIntent);
+                PendingIntent resultPendingIntent =
+                        stackBuilder.getPendingIntent(0,
+                                PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
+
+                //3. Buat builder untuk membuat notifikasi
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(InputNilai.this, CHANNEL_ID)
+                        .setSmallIcon(R.drawable.ic_baseline_notification)
+                        .setContentTitle("UPDATE NILAI")
+                        .setContentText("Baru saja Anda melakukan Update nilai KP Mahasiswa")
+                        .setContentIntent(resultPendingIntent)
+                        .addAction(R.drawable.ic_baseline_notification, "LIHAT", resultPendingIntent)
+                        .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+
+
+                //4. Buat objek notifikasi
+                Notification notification = builder.build();
+
+                //5. Tampilkan notifikasi
+                notificationManager.notify(102, notification);
+            }
+        });
+    }
+
+    //2. Buat channel
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, "Kanal Input Nilai", NotificationManager.IMPORTANCE_HIGH);
+            channel.setDescription("Kanal notifikasi Input Nilai");
+            notificationManager.createNotificationChannel(channel);
+
+        }
     }
 }
