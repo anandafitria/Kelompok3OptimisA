3b92f2abb0422ab96b01f0e77b765ccd6ea40bca
Aisyah2011521018 <91660844+Aisyah2011521018@users.noreply.github.com>
2022-12-08 01:05:56 +0700

    notif login

--- app/src/main/java/com/example/kelompok3optimisa/LoginActivity.java
+++ app/src/main/java/com/example/kelompok3optimisa/LoginActivity.java
@@ -1,8 +1,16 @@
 package com.example.kelompok3optimisa;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,9 +21,11 @@ import android.widget.Toast;
 public class LoginActivity extends AppCompatActivity {
 
 
+    private static final String CHANNEL_ID = "Login_kanal";
     EditText NIDN, Password;
-    Button BtnLogin;
 
+    private Button BtnLogin;
+    private NotificationManagerCompat notificationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +36,14 @@ public class LoginActivity extends AppCompatActivity {
         Password = findViewById(R.id.edit_2);
         BtnLogin = findViewById(R.id.btn_1);
 
-        BtnLogin.setOnClickListener(new View.OnClickListener() {
+        //1. Ambil notifikationManager
+        notificationManager = NotificationManagerCompat.from(this);
+
+        //2b Buat channel notifikasi
+        createNotificationChannel();
+
+        BtnLogin = findViewById(R.id.btn_1);
+        BtnLogin.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
                 //isi action button
@@ -47,8 +64,42 @@ public class LoginActivity extends AppCompatActivity {
 
                 Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                 startActivity(intent);
+
+                Intent resultIntent = new Intent(LoginActivity.this, ProfilActivity.class);
+                TaskStackBuilder stackBuilder = TaskStackBuilder.create(LoginActivity.this);
+                stackBuilder.addNextIntentWithParentStack(resultIntent);
+                PendingIntent resultPendingIntent =
+                        stackBuilder.getPendingIntent(0,
+                                PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
+
+                //3. Buat builder untuk membuat notifikasi
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(LoginActivity.this, CHANNEL_ID)
+                        .setSmallIcon(R.drawable.ic_baseline_notification)
+                        .setContentTitle("INFO LOGIN")
+                        .setContentText("Anda baru saja login menggunakan akun husnil*")
+                        .setContentIntent(resultPendingIntent)
+                        .addAction(R.drawable.ic_baseline_notification, "LIHAT", resultPendingIntent)
+                        .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+
+
+                //4. Buat objek notifikasi
+                Notification notification = builder.build();
+
+                //5. Tampilkan notifikasi
+                notificationManager.notify(101, notification);
             }
         });
+    }
+
+    //2. Buat channel
+    private void createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, "Kanal login", NotificationManager.IMPORTANCE_HIGH);
+            channel.setDescription("Kanal notifikasi Login");
+            notificationManager.createNotificationChannel(channel);
+
+        }
     }
 
 }
